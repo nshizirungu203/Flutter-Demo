29e86a95fc291650256e348e721d3e6042bc2588
Dieudonne NSHIZIRUNGU <nshizirungu203@gmail.com>
2021-12-25 12:27:05 +0200

    Flutter-Demo App

--- lib/cubit/app_cubit_logics.dart
+++ lib/cubit/app_cubit_logics.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cubit/cubit/app_cubit_states.dart';
 import 'package:flutter_cubit/cubit/app_cubits.dart';
+import 'package:flutter_cubit/pages/detail_page.dart';
 import 'package:flutter_cubit/pages/home_page.dart';
+import 'package:flutter_cubit/pages/navpages/main_page.dart';
 import 'package:flutter_cubit/pages/welcome_page.dart';
 
 class AppCubitLogics extends StatefulWidget {
@@ -19,11 +21,14 @@ class _AppCubitLogicsState extends State<AppCubitLogics> {
     return Scaffold(
       body: BlocBuilder<AppCubits, CubitStates>(
         builder: (context, state){
-          if(state is WelcomePage){
+          if(state is DetailState){
+            return DetailPage();
+          }
+          if(state is WelcomeState){
             return WelcomePage();
           }
           if(state is LoadedState){
-            return HomePage();
+            return MainPage();
           }
           if(state is LoadingState){
             return Center(child: CircularProgressIndicator(),);
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cubit/cubit/app_cubit_logics.dart';
 import 'package:flutter_cubit/cubit/app_cubits.dart';
 import 'package:flutter_cubit/pages/detail_page.dart';
+import 'package:flutter_cubit/pages/home_page.dart';
 import 'package:flutter_cubit/pages/navpages/main_page.dart';
 import 'package:flutter_cubit/pages/welcome_page.dart';
 import 'package:flutter_cubit/services/data_services.dart';
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
         create: (context)=>AppCubits(
           data: DataServices(),
         ),
-        child:  AppCubitLogics(),
+        child: AppCubitLogics() ,
       )
     );
   }
